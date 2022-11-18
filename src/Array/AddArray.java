164c34be0fdad68bbd667810d0f77196b9165d02
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-11-18 00:40:59 -0300

    Create AddArray.java

---
+++ src/Array/AddArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class AddArray {
+	public static void main(String[] args) {
+		Integer[] notas = {7,8,6,4,9};
+		
+		for(int i = 0; i < notas.length; i++) {
+			System.out.println(notas[i]);
+		}
+		
+		System.out.println("Total de notas: " + notas.length);
+		
+		
+		
+
+		
+	} 
+	
+}
