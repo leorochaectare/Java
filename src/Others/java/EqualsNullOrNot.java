4876559ff0c864c9b71b629afd327796cc0ef00f
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-01-11 16:55:24 -0300

    Create EqualsNullOrNot.java

---
+++ src/Others/java/EqualsNullOrNot.java
@@ -0,0 +1,12 @@
+package Others.java;
+
+public class EqualsNullOrNot {
+	public static void main(String[] args) {
+		String frase = null;
+		
+		if(frase == null) {
+			System.out.println(true);
+		} 
+		
+	}
+}
