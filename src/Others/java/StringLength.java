3557328e6ab03a724538009e5002a5ebce806506
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-03-20 15:00:35 -0300

    Create StringLength.java

---
+++ src/Others/java/StringLength.java
@@ -0,0 +1,13 @@
+package Others.java;
+
+public class StringLength {
+	public static void main(String[] args) {
+		String valor = "";
+		
+		if(valor.length() > 0 && valor != null) {
+			System.out.println("funcionou");
+		} else {
+			System.out.println("saiu no else");
+		}
+	}
+}
