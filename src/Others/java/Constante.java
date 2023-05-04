115af0f6871f54180de4ce82ba8803ae590399b2
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-05-04 00:14:52 -0300

    Testes

---
+++ src/Others/java/Constante.java
@@ -0,0 +1,12 @@
+package Others.java;
+
+public class Constante {
+	public static void main(String[] args) {
+		
+		
+		final int age = 25;
+		
+		
+		System.out.println(age);
+	}
+}
---
+++ src/Others/java/Teste.java
@@ -0,0 +1,9 @@
+package Others.java;
+
+public class Teste {
+	public static void main(String[] args) {
+		char letra = 'A';
+		
+		System.out.println(letra);
+	}
+}
