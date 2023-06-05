18923c1a5b32bed67ceb9c1356fe234eea09ce08
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-06-05 17:36:45 -0300

    Update Teste.java

--- src/Others/java/Teste.java
+++ src/Others/java/Teste.java
@@ -1,9 +1,12 @@
 package Others.java;
 
+import java.text.DecimalFormat;
+
 public class Teste {
 	public static void main(String[] args) {
-		char letra = 'A';
+		DecimalFormat df = new DecimalFormat("########0.00");
+		Double valor = 100000000000000000.01;
 		
-		System.out.println(letra);
+		System.out.println(df.format(valor));
 	}
 }
