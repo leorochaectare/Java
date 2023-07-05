c66044ba2fdd9b49c31ab9da796669a0325ef8a2
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-07-05 09:47:15 -0300

    Update Teste.java

--- src/Others/java/Teste.java
+++ src/Others/java/Teste.java
@@ -1,14 +1,32 @@
 package Others.java;
 
-import Others.java.objetos.Purchase;
+import java.time.LocalDateTime;
 
 public class Teste {
 
 	public static void main(String[] args) {
 
-		Purchase purchase = null;
+		String cnhFrente = "1";
+		String cnhVerso = "1";
+		String identidadeFrente = "";
+		String identidadeVerso = "1";
 		
-		System.out.println(purchase);
+		if((cnhFrente.isEmpty() || cnhVerso.isEmpty()) && (identidadeFrente.isEmpty() && identidadeVerso.isEmpty())) {
+			System.out.println("Informar CNH fFrente e verso ou informar identidade frente e verso");
+		} else if((cnhFrente.isEmpty() && cnhVerso.isEmpty()) && (identidadeFrente.isEmpty() || identidadeVerso.isEmpty())) {
+			System.out.println("Informar CNH fFrente e verso ou informar identidade frente e verso");
+		} else if((cnhFrente.isEmpty() || cnhVerso.isEmpty()) && (identidadeFrente.isEmpty() || identidadeVerso.isEmpty())) {
+			System.out.println("Informar CNH fFrente e verso ou informar identidade frente e verso");
+		} else {
+			System.out.println("Documentos corretos");
+		}
+		
+		
+		LocalDateTime data = LocalDateTime.now();
+		System.out.println(String.valueOf(data) instanceof String);
+		
+		
+		
 		
 	}
 
