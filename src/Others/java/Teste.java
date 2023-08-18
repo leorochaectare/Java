cdaddc0c832e5884cfa877f8e87a3b7598630c63
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-08-18 00:29:43 -0300

    Update Teste.java

--- src/Others/java/Teste.java
+++ src/Others/java/Teste.java
@@ -8,13 +8,16 @@ public class Teste {
 	
 	static LinkedList<Processos> listaProcessos = new LinkedList<>();
 	
-	public static boolean validaEtapaAnterior(String processoAtual) {
+	public static Boolean validaEtapaAnterior(String processoAtual) {
 		
 		boolean response = true;
 		
 		// encontrar etapa na lista
 		Processos processo = listaProcessos.stream().filter(process -> process.getDescricao().equals(processoAtual)).findFirst().get();
 		System.out.println(processo);
+		if(processo==null) return null;
+		
+		
 		int indiceProcessoAtual = listaProcessos.lastIndexOf(processo);
 		
 		if(indiceProcessoAtual==0) {
